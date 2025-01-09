e4125e2849707310617b1421d7afa2b23328520b
Rizal Abul Fata <rizalabulfata@gmail.com>
2025-01-09 14:22:20 +0700

    (update) change timestamp channel seeder

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -14,85 +14,86 @@ class PaymentMethodSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = now();
         $channels = [
             [
                 'code' => 'gopay',
                 'name' => 'GoPay / GoPay Later',
                 'logo' => 'Gopay (Alt).svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::E_WALLET
             ],
             [
                 'code' => 'qris',
                 'name' => 'QRIS',
                 'logo' => 'QRIS.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::OTHERS
             ],
             [
                 'code' => 'mandiri',
                 'name' => 'Bank Mandiri',
                 'logo' => 'Mandiri.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::BANK
             ],
             [
                 'code' => 'bni',
                 'name' => 'Bank BNI',
                 'logo' => 'BNI.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::BANK
             ],
             [
                 'code' => 'bri',
                 'name' => 'Bank BRI',
                 'logo' => 'BRI.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::BANK
             ],
             [
                 'code' => 'permata',
                 'name' => 'Bank Permata',
                 'logo' => 'Permata Bank (Alt).svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::BANK
             ],
             [
                 'code' => 'cimb',
                 'name' => 'Bank CIMB Niaga',
                 'logo' => 'CIMB Niaga.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::BANK
             ],
             [
                 'code' => 'dana',
                 'name' => 'Dana Digital',
                 'logo' => 'DANA.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::E_WALLET
             ],
             [
                 'code' => 'ovo',
                 'name' => 'OVO',
                 'logo' => 'OVO (New Alt).svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::E_WALLET
             ],
             [
                 'code' => 'linkaja',
                 'name' => 'LinkAja',
                 'logo' => 'LinkAja.svg',
-                'created_at' => now()->toIso8601String(),
-                'updated_at' => now()->toIso8601String(),
+                'created_at' => $now,
+                'updated_at' => $now,
                 'category' => PaymentMethod::E_WALLET
             ],
         ];
