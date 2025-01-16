430bb118c6f85d00ef392585512a16b436a85c19
Rizal Abul Fata <rizalabulfata@gmail.com>
2025-01-17 06:19:20 +0700

    (update) disabled landing midtrans payment state

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/service-details', function () {
     return view('service-details');
 })->name('service-details');
 
-Route::get('midtrans/paymentstate', fn() => view('hotspot/success-payment-landing'));
 Route::group(['prefix' => 'midtrans/payment'], function () {
     Route::get('success', fn() => view('temp.thank-page'));
     Route::get('failed', fn() => view('temp.failed-page'));
