f23368c4691bc4ddb18be745bc138a7cb5c7590e
rizalabulfata <rizalabulfata@gmail.com>
2025-07-27 09:55:35 +0700

    (update) add voucher and payment transactions

---
+++ app/Filament/Resources/Payments/Tables/PaymentsTable.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources\Payments\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PaymentsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('voucher.code')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('total_amount')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('reference_id')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('payment_datetime')
+                    ->dateTime()
+                    ->searchable()
+                    ->sortable(),
+                IconColumn::make('is_cancel')
+                    ->boolean()
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Vouchers/Tables/VouchersTable.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources\Vouchers\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class VouchersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('order_id')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('code')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('expired_at')
+                    ->dateTime()
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('duration')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('duration_type')
+                    ->searchable()
+                    ->sortable(),
+                IconColumn::make('status')
+                    ->boolean()
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('external_link')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('price')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
