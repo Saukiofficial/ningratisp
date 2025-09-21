a40a02e8b88244bcb9963a7389374380279553ac
rizalabulfata <rizalabulfata@gmail.com>
2025-09-21 15:40:15 +0700

    (fix) missing import invoices

--- app/Http/Controllers/Customer/DashboardController.php
+++ app/Http/Controllers/Customer/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use App\Models\Customer\Invoices;
 use App\Models\VirtualAccount;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
