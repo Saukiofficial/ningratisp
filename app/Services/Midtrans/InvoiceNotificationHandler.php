407ed6da0f54564c8ee7ee1eb7668fb0bf4a58e7
rizalabulfata <rizalabulfata@gmail.com>
2025-10-03 10:33:12 +0700

    (update) pipeline process invoice paid

--- app/Services/Midtrans/InvoiceNotificationHandler.php
+++ app/Services/Midtrans/InvoiceNotificationHandler.php
@@ -3,15 +3,19 @@
 namespace App\Services\Midtrans;
 
 use App\Events\CustomerInvoicePaidEvent;
+use App\Jobs\ActivateCustomerInternetJob;
+use App\Jobs\SendWhatsappMessageJob;
 use App\Models\VirtualAccount;
 use App\Services\PaymentAutoService;
 use App\Services\ReceivableService;
+use Closure;
+use Illuminate\Support\Facades\Pipeline;
 
 class InvoiceNotificationHandler implements NotificationHandlerInterface
 {
     public function handle(string $orderId, array $data): bool
     {
-        $va = VirtualAccount::with('paymentMethod', 'invoice.customerPackage.customer',)
+        $va = VirtualAccount::with('paymentMethod', 'invoice.customerPackage.customer')
             ->where('order_id', $orderId)->first();
 
         if (empty($va)) {
@@ -29,14 +33,41 @@ class InvoiceNotificationHandler implements NotificationHandlerInterface
             $va->paymentMethod,
             $invoice
         );
-        if (!empty($payment)) {
-            $va->status = $data['transaction_status'];
-            $va->save();
 
-            event(new CustomerInvoicePaidEvent($va));
+        if (! empty($payment)) {
+
+            Pipeline::send($va)
+                ->through([
+                    function (VirtualAccount $va, Closure $next) use ($data) {
+                        $va->status = $data['transaction_status'];
+                        $va->save();
+
+                        return $next($va);
+                    },
+                    // Dispatch Event for broadcasting
+                    function (VirtualAccount $va, Closure $next) {
+                        event(new CustomerInvoicePaidEvent($va));
+
+                        return $next($va);
+                    },
+                    function (VirtualAccount $va, Closure $next) use ($data) {
+                        ActivateCustomerInternetJob::dispatch($va->invoice->customerPackage->customer, $data);
+
+                        return $next($va);
+                    },
+                    // TODO: Dispatch SendWhatsappMessageJob
+                    // This job requires a Voucher model, but we only have a VirtualAccount for an Invoice.
+                    // We need to determine how to get the relevant Voucher from the VA/Invoice context
+                    // to dispatch SendWhatsappMessageJob::dispatch($voucher).
+
+                ])
+                ->then(function (VirtualAccount $va) {
+                    // Pipeline finished
+                    app(ReceivableService::class)->syncForInvoice($va->invoice);
+                });
         }
-        app(ReceivableService::class)->syncForInvoice($invoice);
 
-        return !empty($payment);
+
+        return ! empty($payment);
     }
 }
